91444ee16597b329908908df4cd61b1b5c6fb2d9
Marcelo Vitoria <marcvit@gmail.com>
2025-08-26 19:30:17 -0300

    Define project's folder structure
    Sources
      - App
      - Core
        - Models
        - Services
      - Features
        - BreedsList
        - BreedsSearch
        - BreedDetail
      - Resources
    Tests
    UITests

---
+++ Sources/App/DogBreedApp.swift
@@ -0,0 +1,18 @@
+//
+//  DogBreedApp.swift
+//  DogBreedApp
+//
+//  Created by Marcelo Vitoria on 26/08/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DogBreedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
