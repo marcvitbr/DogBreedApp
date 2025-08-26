f83eb99968a8f6cab7a6d78b2be8ebf8ff3bac65
Marcelo Vitoria <marcvit@gmail.com>
2025-08-26 15:16:58 -0300

    Initial Commit

---
+++ DogBreedAppTests/DogBreedAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedAppTests.swift
+//  DogBreedAppTests
+//
+//  Created by Marcelo Vitoria on 26/08/2025.
+//
+
+import Testing
+@testable import DogBreedApp
+
+struct DogBreedAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
