b4e1b21936777008abf86fdff13498fb3e043b7d
Marcelo Vitoria <marcvit@gmail.com>
2025-08-31 17:59:46 -0300

    Add breed name to cells on BreedsListView

--- Sources/Features/BreedsList/View/BreedsListView.swift
+++ Sources/Features/BreedsList/View/BreedsListView.swift
@@ -14,7 +14,6 @@ struct BreedsListView: View {
 
     let columns = [
         GridItem(.adaptive(minimum: 100, maximum: 150)),
-
     ]
 
     var body: some View {
@@ -23,15 +22,28 @@ struct BreedsListView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(store.breeds) { breed in
                         Color.clear.overlay(
-                            AsyncImage(url: breed.imageResolvedURL) { phase in
-                                switch phase {
-                                case .success(let image):
-                                    image
-                                        .resizable()
-                                        .scaledToFill()
-                                default:
-                                    Image(systemName: "photo")
+                            ZStack {
+                                AsyncImage(url: breed.imageResolvedURL) { phase in
+                                    switch phase {
+                                    case .success(let image):
+                                        image
+                                            .resizable()
+                                            .scaledToFill()
+                                    default:
+                                        Image(systemName: "photo")
+                                    }
                                 }
+
+                                VStack {
+                                    Spacer()
+                                    Text(breed.name)
+                                        .font(.caption)
+                                        .foregroundColor(.primary)
+                                        .background(Color.primary
+                                            .colorInvert()
+                                            .opacity(0.75))
+                                }
+                                .padding()
                             }
                         )
                         .frame(maxWidth: .infinity)
@@ -44,5 +56,6 @@ struct BreedsListView: View {
         .onAppear {
             store.send(.fetchBreeds)
         }
+        .navigationTitle("Breeds")
     }
 }
